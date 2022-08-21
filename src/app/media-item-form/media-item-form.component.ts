4a0111084a1844e2f4d0d01f579d593289220128
Konrad Kavai <kottix96@gmail.com>
2022-08-21 19:31:51 +0200

    module based form

--- src/app/media-item-form/media-item-form.component.ts
+++ src/app/media-item-form/media-item-form.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'mw-media-item-form',
@@ -7,6 +8,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MediaItemFormComponent implements OnInit {
 
+  form: FormGroup;
+
   onSubmit(mediaItem) {
     console.log(mediaItem);
   }
@@ -14,6 +17,15 @@ export class MediaItemFormComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+    this.form = new FormGroup({
+      medium: new FormControl('Movies'),
+      name: new FormControl('', Validators.compose([
+        Validators.required,
+        Validators.pattern('[\\w\\-\\s\\/]+')
+      ])),
+      category: new FormControl(''),
+      year: new FormControl(''),
+    });
   }
 
 }
