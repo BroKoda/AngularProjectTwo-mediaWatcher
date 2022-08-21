0046c7db602a1ae2bfd5d73f1496940474a40ca1
Konrad Kavai <kottix96@gmail.com>
2022-08-21 19:18:54 +0200

    template driven form

---
+++ src/app/media-item-form/media-item-form.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'mw-media-item-form',
+  templateUrl: './media-item-form.component.html',
+  styleUrls: ['./media-item-form.component.css']
+})
+export class MediaItemFormComponent implements OnInit {
+
+  onSubmit(mediaItem) {
+    console.log(mediaItem);
+  }
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
